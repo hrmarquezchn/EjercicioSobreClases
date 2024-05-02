d85297eca303312272be840d494140b58f0088e7
hrmarquezchn <hrmarquez69@gmail.com>
2024-05-01 22:55:29 -0600

    02

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const Pelicula = require("./Model/Pelicula");
 
-agregarPelicula('El Padrino', 'Francis Ford Coppola', 1972);
-agregarPelicula('Pulp Fiction', 'Quentin Tarantino', 1994);
+const pelicula1 = new Pelicula(1, "El Padrino", "Francis Ford Coppola", "Drama", 1972);
+const pelicula2 = new Pelicula(2, "Pulp Fiction", "Quentin Tarantino", "Crimen", 1994);
+const pelicula3 = new Pelicula(3, "star trek", "turu tururu", "fixion", 1994);
+console.log(Pelicula)
 
-console.log(obtenerPeliculas());
-// Actualizamos la película con ID 1
-actualizarPelicula(1, 'El Padrino II', 'Francis Ford Coppola', 1974);
-console.log(obtenerPeliculas());
+// Obtener película por ID
+const peliculaObtenida = obtenerPeliculaPorId(1);
+console.log(peliculaObtenida); // Imprime la película obtenida
 
-// Eliminamos la película con ID 2
-eliminarPelicula(2);
-console.log(obtenerPeliculas());
+// Actualizar película
+const peliculaActualizada = new Pelicula(1, "El Padrino: Edición remasterizada", "Francis Ford Coppola", "Drama", 1972);
+actualizarPelicula(peliculaActualizada);
+
+// Eliminar película por ID
+eliminarPeliculaPorId(2);
